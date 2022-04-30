7da69edb208e2acc00eb7aca9b469f52604747b6
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2022-04-30 19:51:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,8 +5,21 @@ function setup() {
     
     canvas = createCanvas(550, 550);
     canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
   }
-  
+  function modelLoaded() {
+      console.log('Model Loaded!');
+  }
+  function save_image() {
+      save('image.png');
+  }
   function draw() {
     background(220);
+  }
+  function gotPoses(results) {
+      if (results.length > 0){
+          console.log(results);
+      }
   }
